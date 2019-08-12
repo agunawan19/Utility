7de8f95a91ccd7581ec22e3f4c1afa0c567026c4
Andri Gunawan <andri_gunawan@msn.com>
2019-08-11 17:21:17 -0700

    Refactor parameter name and variables

--- ReportLibrary/ReportLibrary.vb
+++ ReportLibrary/ReportLibrary.vb
@@ -4,24 +4,24 @@
         Public Function GetDictionaryFromMemoField(
             text As String,
             delimiters As String(),
-            Optional includedColumns As HashSet(Of String) = Nothing) As Dictionary(Of String, String) Implements IReportLibrary.GetDictionaryFromMemoField
+            Optional includedFields As HashSet(Of String) = Nothing) As Dictionary(Of String, String) Implements IReportLibrary.GetDictionaryFromMemoField
             Dim keyValuePairs As New Dictionary(Of String, String)()
             Dim pairs As String() = text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)
-            Dim isIncludedColumn As Boolean = True
+            Dim isIncludedField As Boolean = True
 
             For Each pair As String In pairs
                 Dim kvp As String() = pair.Split("=")
 
-                If includedColumns IsNot Nothing Then
-                    isIncludedColumn = includedColumns.Contains(kvp(0))
+                If includedFields IsNot Nothing Then
+                    isIncludedField = includedFields.Contains(kvp(0))
                 End If
 
-                If isIncludedColumn Then
+                If isIncludedField Then
                     keyValuePairs(kvp(0)) = kvp(1)
                 End If
 
-                If includedColumns IsNot Nothing Then
-                    If keyValuePairs.Count = includedColumns.Count Then
+                If includedFields IsNot Nothing Then
+                    If keyValuePairs.Count = includedFields.Count Then
                         Exit For
                     End If
                 End If
