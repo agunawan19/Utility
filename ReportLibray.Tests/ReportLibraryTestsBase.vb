d2d67c4e367812c9310e5d9a5c521438240a5a9f
Andri Gunawan <andri_gunawan@msn.com>
2019-08-17 11:33:46 -0700

    Change property name

--- ReportLibray.Tests/ReportLibraryTestsBase.vb
+++ ReportLibray.Tests/ReportLibraryTestsBase.vb
@@ -3,16 +3,16 @@ Imports Moq
 Imports UtilityLibraries
 
 Public Class ReportLibraryTestsBase
-    Private ReadOnly Property Dependency As IUnityContainer
+    Private ReadOnly Property Container As IUnityContainer
     Public ReadOnly Property MockReportLibrary As Mock(Of IReportLibrary)
     Public ReadOnly Property ReportLibrary As IReportLibrary
 
     Public Sub New()
-        Dependency = New UnityContainer()
-        Dependency.RegisterInstance(GetType(Mock), New Mock(Of IUnityContainer))
-        Dependency.RegisterType(Of IReportLibrary, ReportLibrary)()
-        ReportLibrary = Dependency.Resolve(Of ReportLibrary)()
-        MockReportLibrary = Dependency.Resolve(Of Mock(Of IReportLibrary))()
+        Container = New UnityContainer()
+        Container.RegisterInstance(GetType(Mock), New Mock(Of IUnityContainer))
+        Container.RegisterType(Of IReportLibrary, ReportLibrary)()
+        ReportLibrary = Container.Resolve(Of ReportLibrary)()
+        MockReportLibrary = Container.Resolve(Of Mock(Of IReportLibrary))()
     End Sub
 
 End Class
