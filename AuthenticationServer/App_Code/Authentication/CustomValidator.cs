f7280d6437670570877f85c79074fb1f4b64a062
agunawan <agunawan@ascentis.com>
2019-11-19 16:37:41 -0800

    Add AuthenticationServer WFC project

---
+++ AuthenticationServer/App_Code/Authentication/CustomValidator.cs
@@ -0,0 +1,24 @@
+﻿using AuthenticationServer.Models;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticationServer.App_Code.Authentication
+{
+    public class CustomValidator : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            var accountModel = new AccountModel();
+
+            if (accountModel.IsAuthenticated(userName, password)) {
+                return;
+            }
+
+            throw new SecurityTokenException("Invalid account");
+        }
+    }
+}
---
+++ AuthenticationServer/Models/AccountModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticationServer.Models
+{
+    public class AccountModel
+    {
+        private readonly List<Account> accounts = new List<Account>();
+
+        public AccountModel()
+        {
+            accounts.Add(new Account { Username = "acc1", Password = "123" });
+            accounts.Add(new Account { Username = "acc2", Password = "123" });
+            accounts.Add(new Account { Username = "acc3", Password = "123" });
+        }
+
+        public bool IsAuthenticated(string username, string password)
+        {
+            return accounts.Any(account => account.Username.Equals(username)
+                && account.Password.Equals(password));
+        }
+    }
+}
